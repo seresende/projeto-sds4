52b5b1ddf7cee93ff7897fe63d53bb4ddb6f4071
Sergio Resende <sergio-resende@altamogiana.com.br>
2021-09-08 08:25:27 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
 
 
 function App() {
   return (
-    <div>
-      <h1>Olá mundo</h1>
+    <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary">Olá mundo</h1>
+      
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
